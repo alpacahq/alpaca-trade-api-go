63ac4e59559bfafb9935e65e99b84bf3c685208c
Gabor Lekeny <gabor@alpaca.markets>
2021-11-09 14:09:51 +0100

    Refactor handleSubChange (#171)

--- marketdata/stream/subscription.go
+++ marketdata/stream/subscription.go
@@ -11,120 +11,114 @@ type subChangeRequest struct {
 	result chan error
 }
 
-func newSubChangeRequest() subChangeRequest {
-	return subChangeRequest{
-		result: make(chan error),
-	}
-}
-
 func (sc *stocksClient) SubscribeToTrades(handler func(Trade), symbols ...string) error {
 	sc.handler.mu.Lock()
 	sc.handler.tradeHandler = handler
 	sc.handler.mu.Unlock()
-	return sc.client.handleSubChange(true, newSubChangeRequest(), subscriptions{trades: symbols})
+	return sc.client.handleSubChange(true, subscriptions{trades: symbols})
 }
 
 func (sc *stocksClient) SubscribeToQuotes(handler func(Quote), symbols ...string) error {
 	sc.handler.mu.Lock()
 	sc.handler.quoteHandler = handler
 	sc.handler.mu.Unlock()
-	return sc.client.handleSubChange(true, newSubChangeRequest(), subscriptions{quotes: symbols})
+	return sc.client.handleSubChange(true, subscriptions{quotes: symbols})
 }
 
 func (sc *stocksClient) SubscribeToBars(handler func(Bar), symbols ...string) error {
 	sc.handler.mu.Lock()
 	sc.handler.barHandler = handler
 	sc.handler.mu.Unlock()
-	return sc.client.handleSubChange(true, newSubChangeRequest(), subscriptions{bars: symbols})
+	return sc.client.handleSubChange(true, subscriptions{bars: symbols})
 }
 
 func (sc *stocksClient) SubscribeToDailyBars(handler func(Bar), symbols ...string) error {
 	sc.handler.mu.Lock()
 	sc.handler.dailyBarHandler = handler
 	sc.handler.mu.Unlock()
-	return sc.client.handleSubChange(true, newSubChangeRequest(), subscriptions{dailyBars: symbols})
+	return sc.client.handleSubChange(true, subscriptions{dailyBars: symbols})
 }
 
 func (sc *stocksClient) SubscribeToStatuses(handler func(TradingStatus), symbols ...string) error {
 	sc.handler.mu.Lock()
 	sc.handler.tradingStatusHandler = handler
 	sc.handler.mu.Unlock()
-	return sc.client.handleSubChange(true, newSubChangeRequest(), subscriptions{statuses: symbols})
+	return sc.client.handleSubChange(true, subscriptions{statuses: symbols})
 }
 
 func (sc *stocksClient) SubscribeToLULDs(handler func(LULD), symbols ...string) error {
 	sc.handler.mu.Lock()
 	sc.handler.luldHandler = handler
 	sc.handler.mu.Unlock()
-	return sc.client.handleSubChange(true, newSubChangeRequest(), subscriptions{lulds: symbols})
+	return sc.client.handleSubChange(true, subscriptions{lulds: symbols})
 }
 
 func (sc *stocksClient) UnsubscribeFromTrades(symbols ...string) error {
-	return sc.handleSubChange(false, newSubChangeRequest(), subscriptions{trades: symbols})
+	return sc.handleSubChange(false, subscriptions{trades: symbols})
 }
 
 func (sc *stocksClient) UnsubscribeFromQuotes(symbols ...string) error {
-	return sc.handleSubChange(false, newSubChangeRequest(), subscriptions{quotes: symbols})
+	return sc.handleSubChange(false, subscriptions{quotes: symbols})
 }
 
 func (sc *stocksClient) UnsubscribeFromBars(symbols ...string) error {
-	return sc.handleSubChange(false, newSubChangeRequest(), subscriptions{bars: symbols})
+	return sc.handleSubChange(false, subscriptions{bars: symbols})
 }
 
 func (sc *stocksClient) UnsubscribeFromDailyBars(symbols ...string) error {
-	return sc.handleSubChange(false, newSubChangeRequest(), subscriptions{dailyBars: symbols})
+	return sc.handleSubChange(false, subscriptions{dailyBars: symbols})
 }
 
 func (sc *stocksClient) UnsubscribeFromStatuses(symbols ...string) error {
-	return sc.handleSubChange(false, newSubChangeRequest(), subscriptions{statuses: symbols})
+	return sc.handleSubChange(false, subscriptions{statuses: symbols})
 }
 
 func (sc *stocksClient) UnsubscribeFromLULDs(symbols ...string) error {
-	return sc.handleSubChange(false, newSubChangeRequest(), subscriptions{lulds: symbols})
+	return sc.handleSubChange(false, subscriptions{lulds: symbols})
 }
 
 func (cc *cryptoClient) SubscribeToTrades(handler func(CryptoTrade), symbols ...string) error {
 	cc.handler.mu.Lock()
 	cc.handler.tradeHandler = handler
 	cc.handler.mu.Unlock()
-	return cc.client.handleSubChange(true, newSubChangeRequest(), subscriptions{trades: symbols})
+	return cc.client.handleSubChange(true, subscriptions{trades: symbols})
 }
 
 func (cc *cryptoClient) SubscribeToQuotes(handler func(CryptoQuote), symbols ...string) error {
 	cc.handler.mu.Lock()
 	cc.handler.quoteHandler = handler
 	cc.handler.mu.Unlock()
-	return cc.client.handleSubChange(true, newSubChangeRequest(), subscriptions{quotes: symbols})
+	return cc.client.handleSubChange(true, subscriptions{quotes: symbols})
 }
 
 func (cc *cryptoClient) SubscribeToBars(handler func(CryptoBar), symbols ...string) error {
 	cc.handler.mu.Lock()
 	cc.handler.barHandler = handler
 	cc.handler.mu.Unlock()
-	return cc.client.handleSubChange(true, newSubChangeRequest(), subscriptions{bars: symbols})
+	return cc.client.handleSubChange(true, subscriptions{bars: symbols})
 }
 
 func (cc *cryptoClient) SubscribeToDailyBars(handler func(CryptoBar), symbols ...string) error {
 	cc.handler.mu.Lock()
 	cc.handler.dailyBarHandler = handler
 	cc.handler.mu.Unlock()
-	return cc.client.handleSubChange(true, newSubChangeRequest(), subscriptions{dailyBars: symbols})
+	return cc.client.handleSubChange(true, subscriptions{dailyBars: symbols})
 }
 
 func (cc *cryptoClient) UnsubscribeFromTrades(symbols ...string) error {
-	return cc.handleSubChange(false, newSubChangeRequest(), subscriptions{trades: symbols})
+	return cc.handleSubChange(false, subscriptions{trades: symbols})
 }
 
 func (cc *cryptoClient) UnsubscribeFromQuotes(symbols ...string) error {
-	return cc.handleSubChange(false, newSubChangeRequest(), subscriptions{quotes: symbols})
+	return cc.handleSubChange(false, subscriptions{quotes: symbols})
 }
 
 func (cc *cryptoClient) UnsubscribeFromBars(symbols ...string) error {
-	return cc.handleSubChange(false, newSubChangeRequest(), subscriptions{bars: symbols})
+	return cc.handleSubChange(false, subscriptions{bars: symbols})
 }
 
 func (cc *cryptoClient) UnsubscribeFromDailyBars(symbols ...string) error {
-	return cc.handleSubChange(false, newSubChangeRequest(), subscriptions{dailyBars: symbols})
+	return cc.handleSubChange(false, subscriptions{dailyBars: symbols})
 }
 
 type subscriptions struct {
@@ -143,9 +137,7 @@ func (s subscriptions) noSubscribeCallNecessary() bool {
 
 var timeAfter = time.After
 
-func (c *client) handleSubChange(
-	subscribe bool, request subChangeRequest, changes subscriptions,
-) error {
+func (c *client) handleSubChange(subscribe bool, changes subscriptions) error {
 	if !c.connectCalled {
 		return ErrSubscriptionChangeBeforeConnect
 	}
@@ -157,7 +149,11 @@ func (c *client) handleSubChange(
 	if err != nil {
 		return err
 	}
-	request.msg = msg
+
+	request := subChangeRequest{
+		result: make(chan error),
+		msg:    msg,
+	}
 
 	if err := c.setSubChangeRequest(&request); err != nil {
 		return err
