5369a29a68084cd600b0c6de88f1e6ed54eee5a4
Gergely Novak <novak.r.gergely@gmail.com>
2022-01-20 08:30:49 +0100

    add polygon package doc with deprecation

---
+++ polygon/doc.go
@@ -0,0 +1,7 @@
+/*
+Package polygon is legacy and should not be used at all.
+It used to provide access to polygon data, which is no longer available for Alpaca users.
+
+Deprecated: use Alpaca market data instead.
+*/
+package polygon
