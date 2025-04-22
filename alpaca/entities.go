6e585bf9c8b3314186448a2c9509a7ff57da8e8a
yannickFuereder <yannick.fuereder@gmail.com>
2025-04-22 20:13:36 +0200

    removed depcrecation warning

--- alpaca/entities.go
+++ alpaca/entities.go
@@ -129,7 +129,7 @@ type Asset struct {
 	Shortable                    bool            `json:"shortable"`
 	EasyToBorrow                 bool            `json:"easy_to_borrow"`
 	Fractionable                 bool            `json:"fractionable"`
-	MaintenanceMarginRequirement uint            `json:"maintenance_margin_requirement"` // Deprecated: Please use margin_requirement_long or margin_requirement_short instead. Note that these fields are of type string.
+	MaintenanceMarginRequirement uint            `json:"maintenance_margin_requirement"`
 	MarginRequirementLong        decimal.Decimal `json:"margin_requirement_long"`
 	MarginRequirementShort       decimal.Decimal `json:"margin_requirement_short"`
 	Attributes                   []string        `json:"attributes"`
