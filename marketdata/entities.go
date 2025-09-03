3c89b3fc00a11c552d006f6555e3e9fba4b56dc4
Dasun Nimantha <dasun.nimantha.c@alpaca.markets>
2025-09-03 14:56:35 +0530

    added new crypto feed KRAKEN

--- marketdata/entities.go
+++ marketdata/entities.go
@@ -41,6 +41,7 @@ const (
 	// US is the crypto feed for the United States.
 	US     CryptoFeed = "us"
 	GLOBAL CryptoFeed = "global"
+	KRAKEN CryptoFeed = "krakrn"
 )
 
 // OptionFeed defines the source feed of option data.
